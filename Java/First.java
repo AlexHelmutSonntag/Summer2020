52acb5c2bacaa83bfd53f3444c22e61dda553cc3
AlexHelmutSonntag <61309161+AlexHelmutSonntag@users.noreply.github.com>
2020-06-18 19:21:40 +0200

    Create First.java

---
+++ Java/First.java
@@ -0,0 +1,19 @@
+/*This file is from 00:00 till 16:00
+it is about an introduction to
+/*
+in any java code, there should be at least one function which is the main function.
+This function doesn't exist on it's own but belongs to a class
+Class is a container for related functions 
+And the main class is called main and the function that we describe in it belongs to the class and is called a "Method"
+a "method" is a function that is part of a  class or belongs to it but when a function that doesn't belong to a class it is called a "function"
+
+all these classes and methods should have an access modifier and this determines if other classes and methods can access these classes and methods like : Public, Private and so on.
+why the name of the class is capital because we use the PascalNamingConvention and with methods we use camelNamingConvention
+*/
+ 
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("Go buy Groceries");
+    }
+}
