0f6871b44239d56e3aadbcaf2e17f459ed84ddae
AlexHelmutSonntag <61309161+AlexHelmutSonntag@users.noreply.github.com>
2020-06-21 22:31:30 +0200

    Update Mortgage_calc.java

--- Java/Mortgage_calc.java
+++ Java/Mortgage_calc.java
@@ -9,25 +9,38 @@ public class Mortgage_calc {
         final byte PERCENT = 100;
 
         Scanner scanner = new Scanner(System.in);
+        int principal = 0;
+        while (true) {
+            System.out.print("Principal($1K - $ 1M): ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter a number between 1K and 1M");
+        }
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
-
-        System.out.print("Annual Interest rate: ");
-        // scanner = new Scanner(System.in);
-        float annualInterest = scanner.nextFloat();
+        float annualInterest = 0;
+        while (true) {
+            System.out.print("Annual Interest rate: ");
+            annualInterest = scanner.nextFloat();
+            if (annualInterest > 0 && annualInterest <= 30)
+                break;
+            System.out.println("Enter a number between 1 and 30");
+        }
         float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-
-        System.out.print("Period(Years): ");
-        // scanner = new Scanner(System.in);
-        byte years = scanner.nextByte();
+        byte years = 0;
+        while (true) {
+            System.out.print("Period(Years): ");
+            years = scanner.nextByte();
+            if (years > 0 && years <= 30)
+                break;
+            System.out.println("Enter a number between 1 and 30 ");
+        }
         int numberOfPayments = years * MONTHS_IN_YEAR;
-
         double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
                 / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
 
         String result = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage = " + result);
+        System.out.println("Mortgage: " + result);
     }
 
 }
