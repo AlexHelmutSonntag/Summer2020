9a16cc60746547284af1ac8ee3c6cac5a252e73d
AlexHelmutSonntag <61309161+AlexHelmutSonntag@users.noreply.github.com>
2020-06-29 23:31:40 +0200

    Add files via upload

---
+++ Java/StringReverse.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class StringReverse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        sc.close();
+        int a_pointer = 0;
+        int b_pointer = A.length() - 1;
+        /* Fastest way to do it using a testing algorithm */
+        while (a_pointer <= b_pointer) {
+            if (A.charAt(a_pointer) != A.charAt(b_pointer)) {
+                System.out.println("No");
+                return;
+            }
+            a_pointer++;
+            b_pointer--;
+        }
+        System.out.println("Yes");
+        // Another way to do it but only using the methods
+        String B = new StringBuilder(A).reverse().toString();
+        if (A.equals(B))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+
+    }
+}
