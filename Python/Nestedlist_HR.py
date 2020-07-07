69196cb6a0ffa13117f64c0b0748179751b63833
AlexHelmutSonntag <61309161+AlexHelmutSonntag@users.noreply.github.com>
2020-07-07 16:23:21 +0200

    Delete Nestedlist_HR.py

--- Python/Nestedlist_HR.py
+++
@@ -1,9 +0,0 @@
-database = []
-N = int(input())
-if N >1 and N <=5:
-    database = [[input(), float(input())] for _ in range(N)]
-    marks_only = sorted(list(set([marks for names, marks in database])))
-    second_highest = marks_only[1]
-    print('\n'.join([a for a,b in sorted(database) if b == second_highest]))
-else : 
-    print("Number must be between 2 and 5")
