dbac5279704883e9a5120b9fd0e55bd65fdc8c8d
AlexHelmutSonntag <61309161+AlexHelmutSonntag@users.noreply.github.com>
2020-07-07 16:34:34 +0200

    Update Nestedlist_HR.py

--- Python/Nestedlist_HR.py
+++ Python/Nestedlist_HR.py
@@ -1,4 +1,4 @@
-database = [[input(), float(input())] for _ in range(N)]
+database = [[input(), float(input())] for _ in range(int(input()))]
 marks_only = sorted(list(set([marks for names, marks in database])))
 second_highest = marks_only[1]
 print('\n'.join([a for a,b in sorted(database) if b == second_highest]))
