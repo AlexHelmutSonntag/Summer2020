19f78a27adb27abf85904360a6498bb045828f2e
AlexHelmutSonntag <61309161+AlexHelmutSonntag@users.noreply.github.com>
2020-07-07 16:24:23 +0200

    Delete Nestedlist_HR.py

--- Nestedlist_HR.py
+++
@@ -1,4 +0,0 @@
-database = [[input(), float(input())] for _ in range(N)]
-marks_only = sorted(list(set([marks for names, marks in database])))
-second_highest = marks_only[1]
-print('\n'.join([a for a,b in sorted(database) if b == second_highest]))
